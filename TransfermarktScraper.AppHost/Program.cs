9f34eaca54175abc77f505c6394d173f2b8df08b
j.dearagon.2016 <j.dearagon.2016@alumnos.urjc.es>
2025-02-17 23:32:57 +0100

    Initial scaffold of .NET Aspire Starter App

---
+++ TransfermarktScraper.AppHost/Program.cs
@@ -0,0 +1,10 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.TransfermarktScraper_ApiService>("apiservice");
+
+builder.AddProject<Projects.TransfermarktScraper_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService)
+    .WaitFor(apiService);
+
+builder.Build().Run();

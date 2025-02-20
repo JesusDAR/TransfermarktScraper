ffc2ac21c627071da45b592e8438d01d300d9f3d
j.dearagon.2016 <j.dearagon.2016@alumnos.urjc.es>
2025-02-20 20:28:06 +0100

    Added simple DbContext

---
+++ TransfermarktScraper.Data/Impl/DbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using TransfermarktScraper.Data.Interfaces;
+
+namespace TransfermarktScraper.Data.Impl
+{
+    /// <inheritdoc/>
+    public class DbContext : IDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DbContext"/> class.
+        /// </summary>
+        /// <param name="configuration">The configuration containing the connection settings.</param>
+        public DbContext(IConfiguration configuration)
+        {
+            var connectionString = configuration["DbSettings:ConnectionString"];
+            var databaseName = configuration["DbSettings:DatabaseName"];
+
+            try
+            {
+                var client = new MongoClient(connectionString);
+                _database = client.GetDatabase(databaseName);
+            }
+            catch (MongoConnectionException ex)
+            {
+                throw new Exception("Cannot connect to MongoDB", ex);
+            }
+        }
+
+        /// <inheritdoc/>
+        public IMongoDatabase Database => _database;
+    }
+}
---
+++ TransfermarktScraper.Data/Interfaces/IDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+
+namespace TransfermarktScraper.Data.Interfaces
+{
+    /// <summary>
+    /// Represents the database context for interacting with MongoDB.
+    /// </summary>
+    public interface IDbContext
+    {
+        /// <summary>
+        /// Gets the MongoDB database instance for performing database operations.
+        /// </summary>
+        IMongoDatabase Database { get; }
+    }
+}

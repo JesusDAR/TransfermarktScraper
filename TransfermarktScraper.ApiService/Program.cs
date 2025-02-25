fd4667970085e854272592844d1dd1498003c161
j.dearagon.2016 <j.dearagon.2016@alumnos.urjc.es>
2025-02-25 20:46:34 +0100

    Removed scaffolded code

--- TransfermarktScraper.ApiService/Program.cs
+++ TransfermarktScraper.ApiService/Program.cs
@@ -37,30 +37,9 @@ namespace TransfermarktScraper.ApiService
             // Configure the HTTP request pipeline.
             app.UseExceptionHandler();
 
-            string[] summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
-
-            app.MapGet("/weatherforecast", () =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    (
-                        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        Random.Shared.Next(-20, 55),
-                        summaries[Random.Shared.Next(summaries.Length)]
-                    ))
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast");
-
             app.MapDefaultEndpoints();
 
             app.Run();
-        }
-
-        record WeatherForecast(DateOnly date, int temperatureC, string? summary)
-        {
-            public int TemperatureF => 32 + (int)(temperatureC / 0.5556);
         }
     }
 }
